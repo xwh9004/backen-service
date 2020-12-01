88346d5370adcb9004cbde038c04ecbb50bf7376
xuwuhui <wuhui.xu@newtouch.cn>
2020-12-01 17:40:28 +0800

    product service init

--- product-service/src/main/java/com/example/product/sql/SqlStatements.java
+++ product-service/src/main/java/com/example/product/sql/SqlStatements.java
@@ -11,7 +11,7 @@ package com.example.product.sql;
  */
 public interface SqlStatements {
 
-    final String INSERT_SQL = "INSERT INTO t_product (product_name,product_amount,product_img_url,product_price,status,product_desc,create_time,update_time) VALUES(?,?,null,?,0,null,?,?)";
+    final String INSERT_SQL = "insert into t_order(order_no,product_id,product_amount,product_unit_price,order_total_price, user_id,status,order_desc,create_time,update_time) values(?,?,?,?,?,?,?,?,?,?,?)";
 
 
 }
